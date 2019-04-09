f4005d092d4b25a731104b022a438fb39a626ccf
zhaokai <985144332@qq.com>
2019-04-09 15:55:41 +0800

    test1

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,140 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+    <div class="hello">
+        {{tether}}
+        <div class="search">
+            <div>
+                <Select v-model="base" style="width:200px">
+                    <Option value="eth" key="eth">ETH</Option>
+                    <Option value="btc" key="btc">BTC</Option>
+                </Select>
+                <Input class="period" placeholder="查询间隔" value="5" v-model="period"/>
+            </div>
+        </div>
+        <div class="list">
+            <Table :columns="columns" :data="data"></Table>
+        </div>
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+    import axios from 'axios';
+
+    export default {
+        name: 'HelloWorld',
+        props: {
+            msg: String
+        },
+        data() {
+            return {
+                tether: [],
+                ticker: [],
+                columns: [
+                    {
+                        title: '货币',
+                        key: 'currency'
+                    },
+                    {
+                        title: '市场价(15分钟内)',
+                        key: '15m'
+                    },
+                    {
+                        title: '最终',
+                        key: 'last'
+                    },
+                    {
+                        title: '买入',
+                        key: 'buy'
+                    },
+                    {
+                        title: '卖出',
+                        key: 'sell'
+                    },
+                    {
+                        title: '货币符号',
+                        key: 'symbol'
+                    }
+                ],
+                data: [],
+                base: 'eth',
+                period: 5,
+                timer: null,
+            }
+        },
+        methods: {
+            getTicker() {
+                const vue = this;
+                axios.get(`https://api.blockchain.info/ticker?currency=CNY&base=${this.base}&cors=true`)
+                    .then(function (response) {
+                        console.log(response);
+                        const data = response.data;
+                        Object.keys(data).forEach(key => {
+                            const item = data[key];
+                            item.currency = key;
+                            vue.data.push(item);
+                        });
+                    })
+                    .catch(function (error) {
+                        console.log(error);
+                    });
+            },
+            init(){
+                //clearInterval(this.timer)
+                const vue = this;
+                setInterval(function(){vue.getTicker(vue.base)}, vue.period * 1000);
+            }
+        },
+        watch:{
+            base(){
+                this.init();
+            },
+            period(){
+                this.init();
+            }
+        },
+        created() {
+            //const vue = this;
+            // axios.get('https://api.coincap.io/v2/candles?exchange=poloniex&interval=h8&baseId=ethereum&quoteId=bitcoin')
+            //         .then(function (response) {
+            //           console.log(response);
+            //           vue.tether = response.data.data;
+            //         })
+            //         .catch(function (error) {
+            //           console.log(error);
+            //         });
+            this.getTicker();
+            this.init();
+        },
+    }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+    h3 {
+        margin: 40px 0 0;
+    }
+
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
+
+    a {
+        color: #42b983;
+    }
+    .search{
+
+    }
+    .search .period{
+        width: 100px;
+        margin-left: 20px;
+    }
+    .list {
+        padding: 10px 200px;
+        margin-top: 20px;
+    }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
+import iView from 'iview';
+import 'iview/dist/styles/iview.css';
 import App from './App.vue'
 
+Vue.use(iView);
 Vue.config.productionTip = false
 
 new Vue({
